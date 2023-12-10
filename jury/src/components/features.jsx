a9f1bee29a17dd560696e0f7d443c8d158830fd8
AjnasNB <ajnasnb@gmail.com>
2023-12-10 05:53:54 +0530

    update

--- jury/src/components/features.jsx
+++ jury/src/components/features.jsx
@@ -6,9 +6,7 @@ const Features = () => {
       <div className="w-[1168px] h-[436px] bg-gradient-to-r from-slate-600 to-gray-800 rounded-lg pl-5 backdrop-blur-2xl flex">
         <img src="/images/logo.png" alt="JuryDAO Logo" className="h-[382px] w-[364px] pt-12 pl-12" />
         <div className="w-auto h-[290px] text-sky-100 text-xl font-normal font-['Nunito'] leading-[33.28px] pt-12 px-[135px]">
-          Metis is an open protocol and the Metis Andromeda network is open to any developer or team that wants to launch a DApp, token, or any other kind of project on our EVM-Equivalent platform.
-          <br />
-          Some of these projects may also choose to incorporate the Metis name into their own. However, Metis does not support or endorse any of these DApps, tokens, or projects unless specifically indicated by MetisLabs Foundation and its team members.
+          With JuryDAO, we are coming up with a nonpartisan selection system. The jurors can join to our DAO by a simple Anon Aadhar verification and joining the official Push Channel. This Channel sends out summons to the members of the DAO when a jury formation is initiated. If they accept the duty, they are applicable to mint a soul bounded ERC721 NFT. Holding this NFT is the primary rule for the on-chain governance system. For a particular case proposal, seven governors are randomly selected to leverage unbiased decision making after the final court hearing. The ERC721 tokens of these governors are burned once the proposal is exected. 
         </div>
       </div>
     </div>
--- jury/src/components/solves.jsx
+++ jury/src/components/solves.jsx
@@ -6,48 +6,55 @@ const Solves = () => {
     <div class="w-[450.95px] h-[445.97px] left-[56px] top-[536.86px] absolute origin-top-left rotate-[-62.74deg] bg-fuchsia-500 rounded-full blur-[200px]"></div>
     <div class="w-[319px] h-[425px] left-[210px] top-[200px] absolute">
         <div class="w-[319px] h-[425px] left-0 top-0 absolute bg-neutral-950 bg-opacity-90 rounded-lg border border-teal-400 backdrop-blur-2xl"></div>
-        <div class="w-[278px] left-[20px] top-[231px] absolute text-sky-100 text-base font-normal font-['Nunito'] leading-[23.18px] tracking-tight">Tired of hiking gas fees, and spending tons of money on development to extend limited functionality?</div>
+        <div class="w-[278px] left-[20px] top-[231px] absolute text-sky-100 text-base font-normal font-['Nunito'] leading-[23.18px] tracking-tight">The system employs a nonpartisan approach to jury selection, eliminating the potential influence of certain officials.</div>
         <div class="w-[211px] h-10 left-[53px] top-[340px] absolute">
             <div class="w-[211px] h-10 left-0 top-0 absolute rounded-md border border-teal-400"></div>
             <div class="w-[211px] h-[37px] pl-5 pr-[18px] py-2.5 left-0 top-[1px] absolute justify-center items-center gap-5 inline-flex">
-                <div class="w-[174px] h-[17px] text-teal-400 text-base font-bold font-['Nunito'] uppercase">jOIN OUR ECOSYSTEM</div>
+                <div class="w-[174px] h-[17px] text-teal-400 text-base font-bold font-['Nunito'] uppercase">Learn More</div>
             </div>
         </div>
         <div class="w-[62px] h-[67px] left-[128px] top-[45px] absolute">
         </div>
         <div class="w-[122px] h-[122px] left-[98px] top-[21px] absolute rounded-full border-2 border-teal-400"></div>
-        <div class="left-[39px] top-[181px] absolute"><span className="text-teal-400 text-xl font-bold font-['Poppins']">BLOCKCHAIN PRO</span><span cl="text-teal-400 text-xl font-bold font-['Poppins'] uppercase">jects</span></div>
+        <div class="left-[39px] top-[181px] absolute"><span className="text-teal-400 text-xl font-bold font-['Poppins']">Nonpartisan Jury Selection</span><span cl="text-teal-400 text-xl font-bold font-['Poppins'] uppercase">jects</span></div>
     </div>
     <div class="w-[319px] h-[425px] left-[561px] top-[200px] absolute">
         <div class="w-[319px] h-[425px] left-0 top-0 absolute bg-neutral-950 rounded-lg border border-teal-400"></div>
-        <div class="w-[278px] left-[20px] top-[231px] absolute text-sky-100 text-base font-normal font-['Nunito'] tracking-tight">Too much complexity to manage deployments, permissions, external storage, and DIY integration?
+        <div class="w-[278px] left-[20px] top-[251px] absolute text-sky-100 text-base font-normal font-['Nunito'] tracking-tight">The system employs a nonpartisan approach to jury selection, eliminating the potential influence of certain officials.
     </div>
         <div class="w-[211px] h-10 left-[53px] top-[340px] absolute">
             <div class="w-[211px] h-10 left-0 top-0 absolute rounded-md border border-teal-400"></div>
             <div class="w-[211px] h-[37px] pl-5 pr-[18px] py-2.5 left-0 top-[1px] absolute justify-center items-center gap-5 inline-flex">
-                <div class="w-[174px] h-[17px] text-teal-400 text-base font-bold font-['Nunito'] uppercase">jOIN OUR ECOSYSTEM</div>
+                <div class="w-[174px] h-[17px] text-teal-400 text-base font-bold font-['Nunito'] uppercase">Learn More</div>
             </div>
         </div>
         <div class="w-[122px] h-[122px] left-[98px] top-[21px] absolute rounded-full border-2 border-teal-400"></div>
         <div class="w-[60px] h-[60px] left-[129px] top-[52px] absolute">
         </div>
-        <div class="left-[76px] top-[181px] absolute text-teal-400 text-xl font-semibold font-['Poppins']">APP DEVELOPERS</div>
+        <div class="left-[76px] top-[181px] absolute text-teal-400 text-xl font-semibold font-['Poppins']">Soul-Bounded ERC721 NFTs</div>
     </div>
     <div class="w-[319px] h-[425px] left-[910px] top-[200px] absolute">
         <div class="w-[319px] h-[425px] left-0 top-0 absolute bg-neutral-950 rounded-lg border border-teal-400"></div>
-        <div class="w-[278px] left-[20px] top-[231px] absolute text-sky-100 text-base font-normal font-['Nunito'] tracking-tight">Experiencing high gas fees, slow wait time, and difficult usability for even the simplest transactions?
+        <div class="w-[278px] left-[20px] top-[251px] absolute text-sky-100 text-base font-normal font-['Nunito'] tracking-tight">  Ensures a level of anonymity and data security while validating the identity of jurors
     </div>
         <div class="w-[211px] h-10 left-[53px] top-[340px] absolute">
             <div class="w-[211px] h-10 left-0 top-0 absolute rounded-md border border-teal-400"></div>
             <div class="w-[211px] h-[37px] pl-5 pr-[18px] py-2.5 left-0 top-[1px] absolute justify-center items-center gap-5 inline-flex">
-                <div class="w-[174px] h-[17px] text-teal-400 text-base font-bold font-['Nunito'] uppercase">jOIN OUR ECOSYSTEM</div>
+                <div class="w-[174px] h-[17px] text-teal-400 text-base font-bold font-['Nunito'] uppercase">Learn More</div>
             </div>
         </div>
         <div class="w-[122px] h-[122px] left-[98px] top-[21px] absolute rounded-full border-2 border-teal-400"></div>
         <div class="w-[75.46px] h-[60px] left-[121px] top-[52px] absolute">
         </div>
-        <div class="left-[106px] top-[181px] absolute text-teal-400 text-xl font-semibold font-['Poppins']">EVERYONE
-    </div>
+        <div class="left-[106px] top-[181px] absolute text-teal-400 text-xl font-semibold font-['Poppins']">Anon Aadhar Verification
+         </div>
+         
+    <div class="w-[211px] h-10 left-[53px] top-[340px] absolute">
+            <div class="w-[211px] h-10 left-0 top-0 absolute rounded-md border border-teal-400"></div>
+            <div class="w-[211px] h-[37px] pl-5 pr-[18px] py-2.5 left-0 top-[1px] absolute justify-center items-center gap-5 inline-flex">
+                <div class="w-[174px] h-[17px] text-teal-400 text-base font-bold font-['Nunito'] uppercase">Learn More</div>
+            </div>
+        </div>
     </div>
 </div>
   );
